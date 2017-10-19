9462fd9cdc5ca82a26e8680cbf206783027cff86
NfN Orange <orange@ff4500.red>
2017-10-19 01:46:22 +0100

    I am not convinced this is very scaleable

--- Process.sh
+++ Process.sh
@@ -27,7 +27,19 @@ sed -i \
     -e "s/>//g" \
     .temp
 
+millis_to_stamp(){
+    seek="$1"
+    millis=$(printf %03d "$(bc -l <<< "scale=0;$seek % 1000")")
+    seek=$(bc -l <<< "scale=0;($seek - $millis)/1000")
+    seconds=$(printf %02d "$(bc -l <<< "scale=0;$seek % 60")")
+    seek=$(bc -l <<< "scale=0;($seek - $seconds)/60")
+    minutes=$(printf %02d "$(bc -l <<< "scale=0;$seek % 60")")
+    hours=$(printf %02d "$(bc -l <<< "scale=0;($seek - $minutes)/60")")
+    echo ${hours:=00}:${minutes:=00}:${seconds:=00}.${millis:=000}
+}
+
 # making timestamps into millis since start
+if [ ! $QUIET ]; then echo "[*] Time stamp to millis"; fi
 while read LINE; do
     STAMP=$(sed -e "$LINE"'!d' -e "s/:/./g" .temp | cut -d"|" -f2)
     IFS=. read -a STAMP_EL <<< "$STAMP"
@@ -35,21 +47,25 @@ while read LINE; do
     sed -i "$LINE"'s/$/|'"$MS"'/' .temp
 done < <(sed -n "/|/=" .temp)
 
-TIMEBEFORE="30"
+TEMPVID=$(mktemp)
+if [ ! $QUIET ]; then echo "[*] Millis + word length"; fi
 while read row; do
     IFS="|" read -a stamp <<< "$row"
+    theWord=$(echo $row | cut -d"|" -f 1 | sed -e "s/[^[:alpha:]]//g")
     inputmillis=${stamp[2]}
-    word=${stamp[0]}
-    seek=$(bc -l <<< "$inputmillis - $TIMEBEFORE")
-    millis=$(printf %03d "$(bc -l <<< "scale=0;$seek % 1000")")
-    seek=$(bc -l <<< "scale=0;($seek - $millis)/1000")
-    seconds=$(printf %02d "$(bc -l <<< "scale=0;$seek % 60")")
-    seek=$(bc -l <<< "scale=0;($seek - $seconds)/60")
-    minutes=$(printf %02d "$(bc -l <<< "scale=0;$seek % 60")")
-    hours=$(printf %02d "$(bc -l <<< "scale=0;($seek - $minutes)/60")")
-    stamp=$(echo $hours:$minutes:$seconds.$millis)
-    #ffmpeg -ss "$stamp" -i "$VIDEO" -ss 00:00:00 -t 0.3 -async 1 -c:v mpeg4 -q:v 1 -c:a aac -q:a 100 $word.mp4 
-    ffmpeg -ss "$stamp" -i "$VIDEO" -ss 00:00:00 -t 0.3 -c copy cuts/$word.mp4 
-    #SHA1=$(sha1sum $word.mp4 | cut -d" " -f 1)
-    #mv $word.mp4 cuts/$word-$SHA1.mp4
+    wordLength=$(echo "scale=2;1*`grep "^$theWord," corpus-length.txt | cut -d, -f2`" | bc -l | cut -d. -f 1)
+    endStamp=""
+    startStamp=""
+    if [[ "$wordLength" ]]; then
+        startStamp=$(millis_to_stamp $(( inputmillis - (wordLength /2))) )
+        endStamp=$(millis_to_stamp $wordLength )
+        echo $startStamp,$endStamp,$theWord >> $TEMPVID
+    fi
 done < .temp
+
+if [ ! $QUIET ]; then echo "[*] Video Cutting"; fi
+while read row; do
+    IFS="," read -a stamp <<< "$row"
+    ffmpeg -ss "${stamp[0]}" -i "$VIDEO" -ss 00:00:00 -t ${stamp[1]} \
+    -async 1 -c:v mpeg4 -q:v 1 -c:a aac -q:a 100 "cuts/${stamp[2]}-${stamp[0]//:/-}.mp4" -y
+done < $TEMPVID
---
+++ WordEstimates.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+WORKINGDIR=$(dirname $0)
+pushd $WORKINGDIR
+if [ ! -e corpus.txt ]; then
+    wget -O corpus.txt https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt
+fi
+TEMPSOUND=$(mktemp)
+while read line; do
+    espeak -s 150 -w $TEMPSOUND "$line"
+    DURATION=$(($(mediainfo --Inform="Audio;%Duration%" $TEMPSOUND) - 400))
+    if [ -e corpus-length.txt ]; then 
+        if grep "$line" corpus-length.txt; then
+            DoesExist=0
+            while read entry; do 
+                if [ "$entry" == "$line" ]; then 
+                    : $(( DoesExist += 1 ))
+                fi
+            done < <(grep "$line" corpus-length.txt | cut -d, -f1)
+            if [ "$DoesExist" == "0" ]; then
+                echo $line,$DURATION >> corpus-length.txt
+            fi
+        else
+            echo $line,$DURATION >> corpus-length.txt
+        fi
+
+    else 
+        echo $line,$DURATION >> corpus-length.txt
+    fi
+done < corpus.txt
+popd
