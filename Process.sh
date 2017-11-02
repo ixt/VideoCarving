1673c13a918c31a5e1dcbe1ca7443afed2a08870
NfN Orange <orange@ff4500.red>
2017-11-02 10:17:22 +0000

    Better benchmarks, Added old method back as function

--- Process.sh
+++ Process.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # TODO:
-#   [*]: Move all processing to RAM to help prevent needless disk wear
-#   [*]: More options & finetuning
-#   [*]: Modularise code into functions more & move to more native bash functions
 #   [ ]: JSON info file for every downloaded video
 #   [ ]: More precise mode that auto adjusts a few times
 #   [ ]: Automatically ignore videos that will probably be too short
@@ -59,6 +56,76 @@ millis_to_stamp(){
     echo ${hours:=00}:${minutes:=00}:${seconds:=00}.${millis:=000}
 }
 
+aenas_based_stamps(){
+    echo "" > $TEMPTIMES
+    
+    # Clean Off Header 
+    LANG=$(head "$SUBS" | grep Language | head -1 | cut -d":" -f2 | xargs echo)
+    if [[ "$LANG" == "en" ]]; then
+        if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Cleaning Subs - en (auto generated)"; fi
+        HEADEREND=$(sed -n "/##/=" "$SUBS")
+        tail -n+$(( HEADEREND + 3 )) "$SUBS" >> $TEMPWORDS
+        sed -i -e "/-->/d;s/<c\.color[0-F]*>//g;s/<[0-9.:/c]*>//g;s/ /\n/g;s/\[Music\]//g" $TEMPWORDS
+        sed -i -e "/^$/d" $TEMPWORDS
+    else
+        if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Cleaning Subs - en or other"; fi
+        tail -n+5 "$SUBS" | sed -e "/-->/d;s/\*.*[^*]\*//g;/^$/d" -e "s/[[:space:]]/\n/g" | sed -e "s/[[:punct:]]*//g" -e "/^$/d" >> $TEMPWORDS
+    fi
+    
+    # Make a ordered list of the words in the subtitles
+    sed -i $TEMPWORDS -e "s/[<>]/\n/g"  
+    sed -i $TEMPWORDS -e "/^\([[:space:]]*\|c\|\/c\)$/d;/[:.]/d;s/ //"
+    
+    if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Using Aeneas to realign subs"; fi
+    # Uses aeneas to enforce title syncronisation
+    python -m aeneas.tools.execute_task "$VIDEO" "$TEMPWORDS" "task_language=eng|os_task_file_format=csv|is_text_type=plain" $TEMPTIMES
+}
+
+espeak_based_stamps(){
+    # Clean Off Header 
+    if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Cleaning Subs"; fi
+    sed -n "/##/=" $SUBS
+    tail +$(( `sed -n "/##/=" "$SUBS"` + 3 )) "$SUBS" >> "$TEMPWORDS"
+    
+    # Clean Gunk
+    sed -i -e "/-->/d" \
+        -e "s/<c>//g" \
+        -e "s/<\/c>//g" \
+        -e "s/<c.color[0-F]*[^>]>//g" \
+        -e "s/[[:space:]]//g" \
+        -e "s/>/>\n/g" \
+        "$TEMPWORDS"
+    
+    sed -i \
+        -e "/[^>]$/d" \
+        -e "/^$/d" \
+        -e "s/</|/g" \
+        -e "s/>//g" \
+        "$TEMPWORDS"
+    
+    # making timestamps into millis since start
+    if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Converting Stamps to Millis"; fi
+    while read LINE; do
+        STAMP=$(sed -e "$LINE"'!d' -e "s/:/./g" "$TEMPWORDS" | cut -d"|" -f2)
+        IFS=. read -a STAMP_EL <<< "$STAMP"
+        MS=$(bc -l <<< "(((((${STAMP_EL[0]} * 60) + ${STAMP_EL[1]}) * 60 ) + ${STAMP_EL[2]}) * 1000 ) + ${STAMP_EL[3]}")
+        sed -i "$LINE"'s/$/|'"$MS"'/' "$TEMPWORDS"
+    done < <(sed -n "/|/=" "$TEMPWORDS")
+    
+    if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Adjusting Stamps with approx word lengths"; fi
+    Wc=0
+    while read row; do
+        IFS="|" read -a stamp <<< "$row"
+        theword=$(echo $row | cut -d"|" -f 1 | sed -e "s/[^[:alpha:]]//g")
+        InputMillis=${stamp[2]}
+        WordLength=$(grep "^$theword," corpus-length.txt | cut -d, -f2)
+        StartMillis=$(( InputMillis - (WordLength /2)))
+        EndMillis=$(( StartMillis + WordLength ))
+        : $(( Wc += 1 )) > /dev/null
+        echo "f$(printf %06d "$Wc"),$StartMillis,$EndMillis,\"$theword\"" >> "$TEMPTIMES"
+    done < $TEMPWORDS
+}
+
 main() {
     while getopts 'o:qhrv:s:i:' flag; do
         case ${flag} in
@@ -87,36 +154,15 @@ main() {
         VIDEO="$TEMPDIR/$DOWNLOAD.mp4"
         popd
     fi
-    
-    STARTTIME=$(date +%S)
+
+    STARTTIME=$(date +%s)
     TEMPWORDS=$(mktemp $TEMPDIR/XXXX)
     TEMPTIMES=$(mktemp $TEMPDIR/XXXX)
-
     ID=$(basename $SUBS | sed -e "s/\..*//g")
 
-    echo "" > $TEMPTIMES
-    
-    # Clean Off Header 
-    LANG=$(head "$SUBS" | grep Language | head -1 | cut -d":" -f2 | xargs echo)
-    if [[ "$LANG" == "en" ]]; then
-        if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Cleaning Subs - en (auto generated)"; fi
-        HEADEREND=$(sed -n "/##/=" "$SUBS")
-        tail -n+$(( HEADEREND + 3 )) "$SUBS" >> $TEMPWORDS
-        sed -i -e "/-->/d;s/<c\.color[0-F]*>//g;s/<[0-9.:/c]*>//g;s/ /\n/g;s/\[Music\]//g" $TEMPWORDS
-        sed -i -e "/^$/d" $TEMPWORDS
-    else
-        if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Cleaning Subs - en or other"; fi
-        tail -n+5 "$SUBS" | sed -e "/-->/d;s/\*.*[^*]\*//g;/^$/d" -e "s/[[:space:]]/\n/g" | sed -e "s/[[:punct:]]*//g" -e "/^$/d" >> $TEMPWORDS
-    fi
-    
-    # Make a ordered list of the words in the subtitles
-    sed -i $TEMPWORDS -e "s/[<>]/\n/g"  
-    sed -i $TEMPWORDS -e "/^\([[:space:]]*\|c\|\/c\)$/d;/[:.]/d;s/ //"
-    
-    if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Using Aeneas to realign subs"; fi
-    # Uses aeneas to enforce title syncronisation
-    python -m aeneas.tools.execute_task "$VIDEO" "$TEMPWORDS" "task_language=eng|os_task_file_format=csv|is_text_type=plain" $TEMPTIMES
-    
+    aenas_based_stamps
+    #espeak_based_stamps
+
     SUCESSES=0
     FAILURES=0
     ERRORS=0
@@ -124,23 +170,24 @@ main() {
     TEMPAUDIOFILE=$(mktemp --suffix=.wav $TEMPDIR/XXXX)
     if [ ! $QUIET ]; then printf '\e[1;34m%-6s\e[m\n' "[INFO] Video Cutting"; fi
     for occurance in $(cat $TEMPTIMES); do 
-        # mpv "$VIDEO" --start ${millis[1]} --end ${millis[2]} --really-quiet
     
         stamp[0]=$(millis_to_stamp `cut -d, -f2 <<< "$occurance"`)
         stamp[1]=$(millis_to_stamp $(bc -l <<< "scale=0;`cut -d, -f3 <<< "$occurance"`-`cut -d, -f2 <<< "$occurance"`"))
         stamp[2]=$(cut -d, -f4 <<< "$occurance" | sed -e "s/\"//g")
     
         if grep "^${stamp[2]}$" corpus.txt -q; then
-            # echo "[*] Stamp output: ${stamp[0]} ${stamp[1]} ${stamp[2]}"
+
             ffmpeg -loglevel panic -ss "${stamp[0]}" -i "$VIDEO" -ss 00:00:00 -t ${stamp[1]} \
             -async 1 -c:v mpeg4 -q:v 1 -c:a aac -q:a 100 "${TEMPVIDEOFILE}" -y >/dev/null 2>&1
             ffmpeg -loglevel panic -i "${TEMPVIDEOFILE}" -vn "${TEMPAUDIOFILE}" -y >/dev/null 2>&1
+
             RESULT=$(./RecogniseAudio.pyo --file-name "${TEMPAUDIOFILE}")
+
             if [[ "$RESULT" == "${stamp[2]}" ]]; then
                 : $(( SUCESSES += 1 ))
                 printf '\e[1;32m%-6s\e[m\n' "[SUCC] Match: \"$RESULT\""
                 HASH=$(sha1sum "$TEMPVIDEOFILE" | cut -d" " -f1)
-                cp "$TEMPVIDEOFILE" "cuts/${stamp[2]}+$HASH+$ID.mp4"
+                cp "$TEMPVIDEOFILE" "aenas/${stamp[2]}+$HASH+$ID.mp4"
             elif [[ "$RESULT" == "" ]]; then
                 printf '\e[1;31m%-6s\e[m\n' "[ERRR] No words found in audio"
                 : $(( ERRORS += 1 ))
@@ -153,9 +200,10 @@ main() {
             fi
         fi
     done
-    
+    ENDTIME=$(date +%s) 
     printf '\e[1;0m%-6s\e[m' "Started: $STARTTIME "
-    printf '\e[1;0m%-6s\e[m\n' "Completed: $(date +%S) "
+    printf '\e[1;0m%-6s\e[m' "Took: $(( ENDTIME - STARTTIME )) "
+    printf '\e[1;0m%-6s\e[m\n' "Completed:  "
     printf '\e[1;32m%-6s\e[m' "$SUCESSES Sucesses "
     printf '\e[1;31m%-6s\e[m' "$ERRORS Errors "
     printf '\e[1;30m%-6s\e[m\n' "$FAILURES Failures"
